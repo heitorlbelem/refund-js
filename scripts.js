d7bfe77c03b33f494e28f912b1da48454d9033dc
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-19 14:07:41 -0300

    Finish project

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ const form = document.querySelector('form')
 const amount = document.getElementById('amount')
 const expense = document.getElementById('expense')
 const category = document.getElementById('category')
-
 const expenseList = document.querySelector('ul')
+const expenseCount = document.querySelector('aside header p span')
+const expensesTotal = document.querySelector('aside header h2')
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, '')
@@ -27,6 +28,15 @@ form.onsubmit = (event) => {
 
   createExpense(newExpense)
 }
+
+expenseList.addEventListener('click', (event) => {
+  if(event.target.classList.contains('remove-icon')) {
+    const item = event.target.closest('li')
+    item.remove()
+  }
+
+  updateTotal()
+})
 
 function formatCurrencyBRL(value) {
   value = value.toLocaleString('pt-BR', {
@@ -56,12 +66,53 @@ function createExpense(newExpense) {
     const amountValue = newExpense.amount.toUpperCase().replace('R$', '')
     expenseAmount.innerHTML = `<small>R$</small> ${amountValue}`
 
+    const removeIcon = document.createElement('img')
+    removeIcon.classList.add('remove-icon')
+    removeIcon.setAttribute('src', 'assets/icons/x_bold.svg')
+    removeIcon.setAttribute('alt', 'remover')
+
     expenseItem.append(
-      expenseIcon, expenseInfo, expenseAmount
+      expenseIcon, expenseInfo, expenseAmount, removeIcon
     )
     expenseList.append(expenseItem)
+
+    clearForm()
+    updateTotal()
   } catch(error) {
     alert('Não fio possível atualizar a lista de despesas')
     console.error(error)
   }
+}
+
+function updateTotal() {
+  try {
+    const items = expenseList.children
+    expenseCount.textContent = `${items.length} ${items.length > 1 ? 'despesas': 'despesa'}`
+
+    let total = 0;
+    for (const item of items) {
+      let value = item.querySelector('p').textContent
+      value = parseFloat(value.replace(/[^\d,]/g, '').replace(',', '.'))
+
+      if(isNaN(value)) {
+        throw new Error('não foi possível calcular o total')
+      }
+
+      total += Number(value)
+    }
+
+    total = formatCurrencyBRL(total).toUpperCase().replace('R$', '')
+    expensesTotal.innerHTML = `<small>R$</small> ${formatCurrencyBRL(total)}`
+  } catch (error) {
+    console.log(error)
+    console.log('Não foi possível atualizar o valor total')
+  }
+}
+
+function clearForm() {
+  amount.value = ''
+  expense.value = ''
+  category.value = ''
+
+  expense.focus()
 }
