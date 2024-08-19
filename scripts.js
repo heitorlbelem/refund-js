908c54df699848fc99722c7480b70bd6a1de893b
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-19 00:38:21 -0300

    Adding html content with JS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
+
+const expenseList = document.querySelector('ul')
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, '')
@@ -8,6 +13,21 @@ amount.oninput = () => {
   amount.value = formatCurrencyBRL(value)
 }
 
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date()
+  }
+
+  createExpense(newExpense)
+}
+
 function formatCurrencyBRL(value) {
   value = value.toLocaleString('pt-BR', {
     style: 'currency',
@@ -15,4 +35,33 @@ function formatCurrencyBRL(value) {
   })
 
   return value
+}
+
+function createExpense(newExpense) {
+  try {
+    const expenseItem = document.createElement('li')
+    expenseItem.classList.add('expense')
+
+    const expenseIcon = document.createElement('img')
+    expenseIcon.setAttribute('src', `assets/icons/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute('alt', `${newExpense.category_name}`)
+
+    const expenseInfo = document.createElement('h3')
+    expenseInfo.innerHTML = `
+      ${newExpense.expense}
+      <span>${newExpense.category_name}</span>
+    `
+
+    const expenseAmount = document.createElement('p')
+    const amountValue = newExpense.amount.toUpperCase().replace('R$', '')
+    expenseAmount.innerHTML = `<small>R$</small> ${amountValue}`
+
+    expenseItem.append(
+      expenseIcon, expenseInfo, expenseAmount
+    )
+    expenseList.append(expenseItem)
+  } catch(error) {
+    alert('Não fio possível atualizar a lista de despesas')
+    console.error(error)
+  }
 }
